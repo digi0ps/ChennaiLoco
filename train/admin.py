28fe6e6def19dc25f16ede98bb49bac736af8272
digi0ps <sriru1998@gmail.com>
2017-03-30 12:27:00 +0530

    Added models Train and Station.

--- train/admin.py
+++ train/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from train.models import Train, Station, Status
 # Register your models here.
+
+admin.site.register(Train)
+admin.site.register(Station)
--- train/models.py
+++ train/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Station(models.Model):
+	number = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=50, unique=True)
+	majorStation = models.BooleanField(default=0)
+	locality = models.CharField(max_length=50)
+	pincode = models.IntegerField()
+
+	def __str__(self):
+		return name
+
+
+class Train(models.Model):
+	number = models.CharField(max_length=50)
+	name = models.IntegerField(primary_key=True)
+	avgTime = models.DecimalField(max_digits=5, decimal_places=2)
+	stations = models.ManyToManyField(
+		Station,
+		related_name="stations",
+		through="Status",
+	)
+
+	def __str__(self):
+		return str(self.number)
+
+
+class Status(models.Model):
+	train = models.ForeignKey(Train, on_delete=models.CASCADE)
+	station = models.ForeignKey(Station, on_delete=models.CASCADE)
+	arrival = models.IntegerField()
+	departure = models.IntegerField()
